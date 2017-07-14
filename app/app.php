a629c8971729ce52eac3f5b2f0a66614f22faa6a
Nicole Freed <nicole.freed@gmail.com>
2017-07-14 13:48:40 -0700

    fix typo in app.php

--- app/app.php
+++ app/app.php
@@ -1,6 +1,7 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Stylist.php";
+    require_once __DIR__."/../src/Client.php";
 
     $server = 'mysql:host=localhost:8889;dbname=hair_salon';
     $username = 'root';
@@ -12,6 +13,9 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
 
     $app->get("/", function() use ($app) {
         return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
@@ -57,7 +61,7 @@
     $app->post("/clients", function() use ($app) {
         $name = $_POST['name'];
         $stylist_id = $_POST['stylist_id'];
-        $client = new Client($name, $description, $stylist_id, $id = null);
+        $client = new Client($name, $stylist_id, $id = null);
         $client->save();
         $stylist = Stylist::find($stylist_id);
         return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
